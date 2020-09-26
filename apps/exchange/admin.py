62caf25afdedfd5648d41dfd3e4c491d0facbad8
kouhkan <amirkouhkan1@gmail.com>
2020-09-26 21:26:14 +0330

    Customize admin panel

--- apps/exchange/admin.py
+++ apps/exchange/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Exchange
 
-# Register your models here.
+
+@admin.register(Exchange)
+class ExchangeAdmin(admin.ModelAdmin):
+    list_display = ('symbol_code', 'group', 'latin_symbol',
+                    'persian_symbol')
+    list_filter = ('group', 'board', 'created', 'status')
+    list_editable = ('status', )
+    search_fields = ('symbol_code', 'latin_symbol', 'latin_name',
+                     'persian_symbol', 'persian_name')
+    list_per_page = 20
+    
