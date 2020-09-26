dabcca1a07793b46e21a72fb44b45bf5d759a3a6
kouhkan <amirkouhkan1@gmail.com>
2020-09-26 19:53:06 +0330

    Create needed files and customized django dirs

--- A/urls.py
+++ A/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # APPs urls
+    path('api/v1/', include([
+        path(
+            'exchange/',
+            include('apps.exchange.api.v1.urls'),
+            name='exchange'
+        ),
+    ])),
+
 ]
--- manage.py
+++ manage.py
@@ -3,19 +3,25 @@
 import os
 import sys
 
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+from pathlib import Path
 
 
 if __name__ == '__main__':
-    main()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?")
+        raise
+
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / 'apps'))
+
+    execute_from_command_line(sys.argv)
