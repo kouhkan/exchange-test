7d8c1d10da49530bcfa7b64aaaf05dbeaa116ed1
kouhkan <amirkouhkan1@gmail.com>
2020-09-27 16:11:48 +0330

    Add swagger to project

--- A/urls.py
+++ A/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +33,24 @@ urlpatterns = [
         ),
     ])),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Exchange API",
+            default_version='v1',
+            description="This is a Test API for Exchanges",
+            contact=openapi.Contact(email="amirkouhkan1@gmail.com"),
+            license=openapi.License(name="MIT"),
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    )
+    urlpatterns += [
+        # SWAGGER
+        path('swagger/json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path('swagger/ui/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('swagger/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    ]
